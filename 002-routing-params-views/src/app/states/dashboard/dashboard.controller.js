c74c5ade6346bfa5e5c33b5a433b7f60b6dd5254
Maria Paz Muñoz <maria.paz@talosdigital.com>
2017-04-04 17:01:48 -0500

    002 EX: HTML-CSS

---
+++ 002-routing-params-views/src/app/states/dashboard/dashboard.controller.js
@@ -0,0 +1,22 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('angularEx1')
+        .controller('DashboardController', DashboardController);
+
+    /** @ngInject */
+    function DashboardController($state, $location, $stateParams, $log, SITE_NAME) {
+        var vm = this;
+        vm.mainTitle = SITE_NAME;
+
+        vm.userName = $stateParams.username;
+        vm.userPw = $stateParams.pass;
+
+        if (vm.userName === null && vm.userPw === null){
+            $state.go('home');
+            $location.url($location.path());
+        }
+
+    }
+})();
