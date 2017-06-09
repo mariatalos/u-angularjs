cd2dee5952768ef7eff9356c513e4a5107f40bb5
Maria Paz Muñoz <maria.paz@talosdigital.com>
2017-06-09 09:41:21 -0500

    007: e2e tests done.

--- 007-end-to-end/e2e/main.po.js
+++ 007-end-to-end/e2e/main.po.js
@@ -7,9 +7,19 @@
 
 var MainPage = function() {
 
+  this.icon = element(by.xpath('//*[@id="site"]/section/main/section/div/form/md-content/div[1]/div/button[3]'));
   this.title = element(by.model('homeCtrl.card.title'));
-  this.directiveTitle = element(by.css('#cardTitle'));
+  this.background = $$('.md-color-picker-input').first();
+  this.textColor = $$('.md-color-picker-input').last();
+  this.description = element(by.model('homeCtrl.card.description'));
+  this.setFavorite = element(by.id('favoriteBtn'));
 
+  this.favoriteList = element.all(by.repeater('favorite in homeCtrl.favoriteList'));
+
+  this.directive = element(by.id('card'));
+  this.directiveReservedBy = element(by.id('cardReservedBy'));
+  this.directiveTitle = element(by.id('cardTitle'));
+  this.directiveDescription = element(by.id('cardDescription'));
 };
 
 module.exports = new MainPage();
